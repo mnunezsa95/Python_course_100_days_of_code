79eefe381310054076888554a4081022c74d9aac
mnunezsa <mnunezsa95@gmail.com>
2024-01-06 11:52:57 -0500

    update day 1

--- day_1.py
+++ day_1.py
@@ -2,5 +2,23 @@
 #                Section 1: Day 1 - Working with variables in Python to manage data                #
 # ------------------------------------------------------------------------------------------------ #
 
-# Print function
-print("Hello, World!")
+### print() function
+print("Hello, World!") # print() function prints to the console
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')") # Using different types of outside quotes to print the inner quotes
+
+### String manipulation
+print("Hello, World!\nHello, World!\nHello, World!") # \n breaks a new line
+
+### String concatenation
+print("Hello" + "Marlon") # + will add two strings together
+print("Hello" + " Marlon") # manually adding a space
+
+### input() function
+input("What is your name? ") # input() function allows for user input in the terminal
+print("Hello " + input("What is your name? ") + "!") # Nesting input() inside of print()
+
+### Variables
+name = input("What is your name?") # assigning the result of the input to a var
+print(name) # var can be accessed later on
