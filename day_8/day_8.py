88ac30b115c58ab97c6204f9146d2cdae520f792
mnunezsa <mnunezsa95@gmail.com>
2024-01-11 17:17:21 -0500

    day_9 continue

--- day_8/day_8.py
+++ day_8/day_8.py
@@ -2,41 +2,50 @@
 #                                        Function Parameters                                       #
 # ------------------------------------------------------------------------------------------------ #
 
-### Functions with inputs
-#--Functions are helpful for performing repetitive actions
-#--Parameters are variables used when defining the function
-#--Arguments are the values of the data used when the function is called
+# Functions with inputs
+# --Functions are helpful for performing repetitive actions
+# --Parameters are variables used when defining the function
+# --Arguments are the values of the data used when the function is called
 
 # functions begin with the def keyword
-def greet(name, birth_country, current_city): # parameters allow for the param to be passed into function for use
+# parameters allow for the param to be passed into function for use
+def greet(name, birth_country, current_city):
     print(f"My name is {name}.")
     print(f"I am from {birth_country}.")
     print(f"I live in {current_city}.")
-    
-greet("Marlon", "Costa Rica", "Boston") # calling the function with the arguments
 
-### Positional and Keyword Arguments
-#--Positional arguments are arguments passed in order to the parameters when calling the function
-#--Keyword arguments are arguments assigned to the parameter name when calling the function
+
+# calling the function with the arguments
+greet("Marlon", "Costa Rica", "Boston")
+
+# Positional and Keyword Arguments
+# --Positional arguments are arguments passed in order to the parameters when calling the function
+# --Keyword arguments are arguments assigned to the parameter name when calling the function
 
 # Using keyword arguments
+
+
 def greet_with_keyword_arguments(name, birth_country, current_city):
     print(f"My name is {name}.")
     print(f"I am from {birth_country}.")
     print(f"I live in {current_city}.")
 
-greet(current_city="Boston", name = "Harper", birth_country = "USA")
+
+greet(current_city="Boston", name="Harper", birth_country="USA")
 
 # Example 1: Prime Number Checker
-def prime_checker(number): # function for checking if a number is prime
-  is_prime = True # variable initialized as true
-  for i in range(2, number): # looping to every number between 2 and 'number'
-    if number % i == 0: # if the number is divisible by any number starting from 2
-      is_prime = False # set is_prime to false
-  if is_prime: # if is_prime is true, print a statement
-    print("It's a prime number.")
-  else: # if is_prime is false, print a statement
-    print("It's not a prime number.")
-    
-prime_checker(4) # Output: It's not a prime number.
-prime_checker(5) # Output: It's a prime number.
+
+
+def prime_checker(number):  # function for checking if a number is prime
+    is_prime = True  # variable initialized as true
+    for i in range(2, number):  # looping to every number between 2 and 'number'
+        if number % i == 0:  # if the number is divisible by any number starting from 2
+            is_prime = False  # set is_prime to false
+    if is_prime:  # if is_prime is true, print a statement
+        print("It's a prime number.")
+    else:  # if is_prime is false, print a statement
+        print("It's not a prime number.")
+
+
+prime_checker(4)  # Output: It's not a prime number.
+prime_checker(5)  # Output: It's a prime number.
--- day_9/day_9.py
+++ day_9/day_9.py
@@ -2,18 +2,122 @@
 #                            Dictionaries & Nesting Inside Dictionaries                            #
 # ------------------------------------------------------------------------------------------------ #
 
-### Dictionaries
-#-Dictionaries have keys-value pairs
-#--Keys serve as the reference point for the value
-#--Values is the data
+# Dictionaries
+# -Dictionaries have keys-value pairs
+# --Keys serve as the reference point for the value
+# --Values is the data
 
 # Syntax
-#--variable name, followed by an assignment operator and curly braces
-#--key/value pairs are separated by comma
+{"key1", "value1", "key2", "value2"}
+# --variable name, followed by an assignment operator and curly braces
+# --key/value pairs are separated by comma
 
 programming_dictionary = {
-    "Bug": "An error in a program that prevents the program from running as expected.", 
+    "Bug": "An error in a program that prevents the program from running as expected.",
     "Function": "A piece of code that you can easily call over and over again.",
-    "Loop": "The action of doing something over and over again"
+    "Loop": "The action of doing something over and over again",
 }
 
+# -Accessing items in a dictionary
+# --When accessing a value by its key, the correct data type needs to be passed into the brackets
+# dictionaries are accessed by their key in square brackets (key needs to be spelled correctly)
+print(programming_dictionary["Bug"])
+
+# -Adding new items to a dictionary
+# set the key name equal to the value
+programming_dictionary[
+    "Python"
+] = "A programming language used for backend development and data analysis"
+print(programming_dictionary)
+
+# -Creating an empty dictionary
+empty_dictionary = {}  # set a variable equal to an empty dictionary
+
+# -Wiping an existing dictionary
+dictionary1 = {1: "one", 2: "two", 3: "three"}  # creating a dictionary
+dictionary1 = {}  # setting an existing dictionary equal to an empty one will wipe it
+print(dictionary1)
+
+# -Editing an item in a dictionary
+# --Setting an existing key equal to a new value will update it
+programming_dictionary[
+    "Bug"
+] = "An error in a program that prevents the program from running as expected. Named after a moth that entered a computer"
+print(programming_dictionary)
+
+# -Looping through a dictionary
+for key in programming_dictionary:  # using a for loop to loop through a dictionary
+    print(key)
+    print(programming_dictionary[key])
+
+# Example 1: grading program
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+
+student_grades = {}
+
+for key in student_scores:
+    score = student_scores[key]
+    if score >= 90:
+        student_grades[key] = "Outstanding"
+    elif score > 80:
+        student_grades[key] = "Exceeds Expectations"
+    elif score > 70:
+        student_grades[key] = "Acceptable"
+    else:
+        student_grades[key] = "Fail"
+
+# Nesting Lists and Dictionaries
+# --Lists and Dictionaries can be nested inside a dictionary
+{"key1": ["value1", "value2"], "key2": {"value3", "value4"}}
+
+capitals = {"France": "Paris", "Germany": "Berlin"}
+
+# --Nesting lists in a dictionary
+travel_log = {
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+}
+
+# --Nesting dictionaries in dictionaries
+travel_log2 = {
+    "France": {
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12,
+    },
+    "Germany": {
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 10,
+    },
+}
+
+# --Nesting dictionaries inside a list
+travel_log3 = [
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12,
+    },
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 10,
+    },
+]
+
+
+# Example 2: Creating a function to add cities
+def add_new_country(country, visits, list_of_cities):
+    travel_log3.append({"country": country, "visits": visits, "cities": list_of_cities})
+
+
+# Adding item
+add_new_country("India", 3, ["Kochi", "Hyderabad", "Bangalore"])
+add_new_country("Japan", 4, ["Kyoto", "Kagoshima", "Tokyo"])
+
+print(travel_log3)
