8db0bb75a69760d15607a078677c5f7b1c702bb1
mnunezsa <mnunezsa95@gmail.com>
2024-01-11 15:48:00 -0500

    start day 9

---
+++ day_9/day_9.py
@@ -0,0 +1,19 @@
+# ------------------------------------------------------------------------------------------------ #
+#                            Dictionaries & Nesting Inside Dictionaries                            #
+# ------------------------------------------------------------------------------------------------ #
+
+### Dictionaries
+#-Dictionaries have keys-value pairs
+#--Keys serve as the reference point for the value
+#--Values is the data
+
+# Syntax
+#--variable name, followed by an assignment operator and curly braces
+#--key/value pairs are separated by comma
+
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.", 
+    "Function": "A piece of code that you can easily call over and over again.",
+    "Loop": "The action of doing something over and over again"
+}
+
