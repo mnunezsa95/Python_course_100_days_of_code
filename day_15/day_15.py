15a882392e7685ee323c175ef8d7d7497352716a
mnunezsa <mnunezsa95@gmail.com>
2024-01-18 20:20:24 -0500

    start day 15

---
+++ day_15/day_15.py
@@ -0,0 +1,22 @@
+# ------------------------------------------------------------------------------------------------ #
+#                                Local Development Environment Setup                               #
+# ------------------------------------------------------------------------------------------------ #
+
+### Installing Python on local computer
+# 1 - Site to download: https://www.python.org/downloads/
+# 2 - Follow Prompts
+
+### Downloading PyCharm for Mac / Windows
+# 1 - Site to download: https://www.jetbrains.com/pycharm/download/?section=mac
+# 2 - Choose community version
+
+### Steps for installing PyCharm on Windows:
+# 1 - https://www.udemy.com/course/100-days-of-code/learn/lecture/19846920#overview
+
+### Steps for installing PyCharm on Mac:
+# 1 - Drag .dmg into application
+
+### Setting up environment
+# 1 - Select project name
+# 2 - Select location
+# 3 - Select latest version of python
