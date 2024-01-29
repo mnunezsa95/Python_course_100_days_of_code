f578823e3c4777aad096c64fc0b6f6d3778d0517
mnunezsa <mnunezsa95@gmail.com>
2024-01-29 16:57:21 -0500

    update

--- day_22/day_22_project.py
+++ day_22/day_22_project.py
@@ -2,21 +2,11 @@
 #                                Build Pong: The Famous Arcade Game                                #
 # ------------------------------------------------------------------------------------------------ #
 
-"""
-Steps
-1) Create the screen
-2) Create and mnove a paddle
-3) Create another paddle
-4) Create the ball and make it move
-5) Detect collision with the wall and bounce
-6) Detect collision with paddle
-7) Detect when paddle misses
-8) Keep score
-"""
 
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -28,6 +18,7 @@ screen.setup(width=800, height=600)
 l_paddle = Paddle((-350, 0))
 r_paddle = Paddle((350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 
@@ -38,12 +29,31 @@ screen.onkey(r_paddle.go_down, "Down")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
     # Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+    # Detect collision with r_paddle
+    if (
+        ball.distance(r_paddle) < 50
+        and ball.xcor() > 320
+        or ball.distance(l_paddle) < 50
+        and ball.xcor() < -320
+    ):
+        ball.bounce_x()
+
+    # Detect when R paddle misses
+    if ball.xcor() > 380:
+        ball.reset()
+        scoreboard.l_point()
+
+    # Detect when L paddle misses
+    if ball.xcor() < -380:
+        ball.reset()
+        scoreboard.r_point()
 
 screen.exitonclick()
