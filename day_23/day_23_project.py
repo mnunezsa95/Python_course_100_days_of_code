03920063366cdcc0b31ca2682f5cb00eaf8074f1
mnunezsa <mnunezsa95@gmail.com>
2024-01-30 09:25:28 -0500

    start day 23

---
+++ day_23/day_23_project.py
@@ -0,0 +1,42 @@
+# ------------------------------------------------------------------------------------------------ #
+#                                       Turtle Crossing Game                                       #
+# ------------------------------------------------------------------------------------------------ #
+
+"""
+1) Move the turtle with keypress
+2) Create and move the cars
+3) Detect collision with car
+4) Detect when turtle reaches other side
+5) Create scoreboard
+"""
+
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+screen.listen()
+
+player = Player()
+car_manager = CarManager()
+
+screen.onkey(player.go_up, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # Detect turtle collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+screen.exitonclick()
