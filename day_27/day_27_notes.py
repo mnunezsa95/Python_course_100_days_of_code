ecc2197c38126c940b96b6f3bcefbf9c9ee687f7
mnunezsa <mnunezsa95@gmail.com>
2024-02-04 18:31:53 -0500

    notes for day 27 done

--- day_27/day_27_notes.py
+++ day_27/day_27_notes.py
@@ -9,14 +9,37 @@
 
 import tkinter
 
+
+def button_clicked():  # callback function
+    print("I got clicked")
+    new_text = input.get()
+    my_label["text"] = new_text  # accessing & changing the text property
+
+
 ### Creating a window
 window = tkinter.Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)  # Adding padding to the edges of program
 
-### Creating a label
+###* Creating a label
 my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold", "italic"))
-my_label.pack(side="left")
+my_label.config(text="New Text")
+my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)  # Adding padding to a specific widget
+
+###* Creating a button
+# command tells button to listen for the callback function
+button = tkinter.Button(text="Click Me", command=button_clicked)
+button.grid(column=1, row=1)
+
+new_button = tkinter.Button(text="New Button")
+new_button.grid(column=2, row=0)
+
+###* Creating an Entry
+input = tkinter.Entry(width=10)
+input.get()  # Returns the input as a string
+input.grid(column=3, row=2)
 
 
 window.mainloop()
@@ -29,9 +52,11 @@ window.mainloop()
 # -- This can be changed when calling the function
 
 
-# -------------------------------------- Unlimited Arguments ------------------------------------- #
-#### Unlimited Arguments (`*args`)
-# - *args is passed into the function's parameter to allow for unlimited arguments
+# -------------------------------- Unlimited Positional Arguments -------------------------------- #
+#### Unlimited Positional Arguments (*args)
+# - *args is passed into the function's parameter to allow for unlimited positional arguments
+# - *args is of type 'tuple'
+# - *args can be accessed using bracket notation (since `*args*` is a tuple)
 
 
 def add(*args):  # the asterik (*) tells python to accept any number of args
@@ -42,3 +67,42 @@ def add(*args):  # the asterik (*) tells python to accept any number of args
 
 
 print(add(1, 2, 3, 4))
+
+
+# ---------------------------------- Unlimited Keyword Arguments --------------------------------- #
+#### Unlimited Keyword Arguments (**kwargs)
+# - **kwargs is passed into the function's parameter to allow for unlimited keyword arguments
+# - **kwargs is of type 'dict'
+# - **kwargs can be accessed using bracket notation (since *args is a dict)
+
+
+def calculate(**kwargs):
+    print(kwargs)
+    for key, value in kwargs.items():
+        print(key)
+        print(value)
+    print(kwargs["add"])
+
+
+calculate(add=3, multiply=5)
+
+
+### Creating classes with **kwargs
+class Car:
+    def __init(self, **kwargs):
+        self.make = kwargs.get("make")
+        self.model = kwargs.get("model")
+        self.color = kwargs.get("color")
+        self.color = kwargs.get("seats")
+
+
+my_car = Car(make="Nissan", model="GT-R")
+print(my_car.model)
+
+
+### Quiz Question: Default Args, *args, **kwargs
+def all_aboard(a, *args, **kw):
+    print(a, args, kw)
+
+
+all_aboard(4, 7, 3, 0, x=10, y=64)  # 4, (7, 3, 0) {"x": 10, "y": 64}
